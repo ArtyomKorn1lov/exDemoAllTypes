6ca367a4673e73ee41962699e40de8e354d5375c
ArtyomKorn1lov <temka.kornilov@mail.ru>
2023-01-23 11:21:30 +0300

    Добавил метаданные для раздела партнёрам

--- partnyeram/.section.php
+++ partnyeram/.section.php
@@ -1,6 +1,8 @@
 <?
 $sSectionName = "Партнёрам";
 $arDirProperties = Array(
+   "description" => "Партнерам. Как стать партнером лидера отрасли",
+   "keywords" => "стать партнером, лидер отрасли",
    "title" => "partners",
    "keywords_inner" => "partners"
 );
