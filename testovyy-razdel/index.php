430e65f8d998c28920d962e05f23a07fae512cfb
Артём Корнилов <temka.kornilov@mail.ru>
2023-01-24 17:15:07 +0300

    Добавил блок с телефоном

--- testovyy-razdel/index.php
+++ testovyy-razdel/index.php
@@ -2,19 +2,11 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тестовый раздел");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:menu",
-	"bot_menu",
+	"bitrix:main.include",
+	"",
 	Array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"COMPONENT_TEMPLATE" => "vertical_multilevel",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "1",
-		"MENU_CACHE_GET_VARS" => "",
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "bottom",
-		"USE_EXT" => "N"
+		"AREA_FILE_SHOW" => "page",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => ""
 	)
 );?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
