bf515adbb71492620a5b7fb69d7add0ad3ce6d34
Артём Корнилов <temka.kornilov@mail.ru>
2023-01-24 16:56:23 +0300

    Добавил bot меню в footer

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -14,14 +14,23 @@
         <nav class="main-menu">
             <div class="item">
                 <div class="title-block">О магазине</div>
-                <ul>
-                    <li><a href="">Отзывы</a>
-                    </li>
-                    <li><a href="">Руководство </a>
-                    </li>
-                    <li><a href="">История</a>
-                    </li>
-                </ul>
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:menu",
+                    "bot_menu",
+                    Array(
+                        "ALLOW_MULTI_SELECT" => "N",
+                        "CHILD_MENU_TYPE" => "left",
+                        "COMPONENT_TEMPLATE" => "vertical_multilevel",
+                        "DELAY" => "N",
+                        "MAX_LEVEL" => "1",
+                        "MENU_CACHE_GET_VARS" => "",
+                        "MENU_CACHE_TIME" => "3600",
+                        "MENU_CACHE_TYPE" => "A",
+                        "MENU_CACHE_USE_GROUPS" => "Y",
+                        "ROOT_MENU_TYPE" => "bottom",
+                        "USE_EXT" => "N"
+                    )
+                );?><br>
             </div>
             <div class="item">
                 <div class="title-block">Каталог товаров</div>
